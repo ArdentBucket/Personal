9f8093fe789653f157355ad8713508c6d19ee8cb
Jaylen LaPrade <jaylenxlap@gmail.com>
2026-08-30 12:58:39 -0700

    Add files via upload

---
+++ C++ Personal Projects/Cht1.cpp
@@ -0,0 +1,90 @@
+# include <iostream>
+# include <vector>
+///////////////////////////// typedef/using /////////////////////////////
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+using text_t = std::string;
+using number_t = int;
+///////////////////////////// NAMESPACE /////////////////////////////
+    //namespace = provides a solution preventing name conflicts
+    //namespace can allows for identically names entities
+    //as long as the namespacees are different. 
+
+namespace first {
+    int x = 1;
+}
+namespace second {
+    int x = 2;
+}
+namespace CUZage {
+    int x = 31;
+}
+int main(){
+
+///////////////////////////// VARIABLES /////////////////////////////
+
+    //integer (Whole Number)
+    int age = 31;
+    int year = 2026;
+    //double (Number including decimal
+    double days = 7.5; 
+    
+
+    //single character (Use single quotes '')
+    char grade = 'A';
+    char initial = 'C';
+    char currency = '$';
+
+    //boolean (True or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = false;
+
+    //string (Objexts that represents a sequence of text)
+    std::string name = "cuh";
+    std::string day = "Thursday";
+    std::string food = "burburs";
+    std::string address = "1987 freddy fabear st.";
+
+    //std::cout << "Hewwo " << name << "\n" << "Your fatass likes to eat " << food << "😭✌️";
+
+
+///////////////////////////// CONSTANT /////////////////////////////
+
+
+    //constwill specify that a value is constant
+    //tells the compiler to prevent anything from modifying the value
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    const int WIDTH = 1920;
+    const int HIGHT = 1080;
+
+    //std::cout << circumference << "cm";
+
+///////////////////////////// NAMESPACE /////////////////////////////^^^
+    int x = 0;
+
+    //std::cout << first::x << "\n";
+
+
+///////////////////////////// typedef/using /////////////////////////////
+
+// keword used to create an additional name (alias) for another data type
+// new identifier for an existing type, reduces typos (include <vector> introduced)
+
+
+    text_t firstname = "CHUZ";
+    number_t CUZage = 31;
+
+    std::cout << firstname << ", and their age is ";
+    std::cout << CUZage << "\n";
+    
+
+}
+
+
+
+
---
+++ C++ Personal Projects/Cht2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main(){
+///////////////////////////// arethamitics /////////////////////////////
+//                  Order of the arethamitics
+//                   1. parenthesis
+//                   2. Multiplications & division
+//                   3. addition and subtraction
+
+    //int students = 6 - (5 + 4) * 3 / 2;
+
+    //students = students + 1;
+    //students+=1;
+    //students++;
+
+    //students = students - 1;
+    //students-=1;
+    //students--;
+
+    //students = students * 2;
+    //students*=2;
+    
+    //students = students / 2;
+    //students/=2;
+
+    //int remainder = students % 3;         ///   % = modulus/remainder
+
+    //std::cout << students;
+
+///////////////////////////// type conversion /////////////////////////////
+// type convirsion converts a value of one data type to another
+//                  Implicit = automatic
+//                  Explicit = Precede value with new data type (int)
+
+//    int x = 3.14;               //Implicitly
+
+//    double x = (int) 3.14;      //Explicitly using '(int)'
+
+//    std::cout << x;
+
+//    std::cout << (char) 100;
+
+    int correct = 32;
+    int questions = 50;
+    double score = correct/(double)questions * 100;     // (double) is used to make sure value isn't removed/truncated from "questions"
+                                                        // from being an int not a double
+    std::cout << score << "%";
+
+    return 0;
+}
